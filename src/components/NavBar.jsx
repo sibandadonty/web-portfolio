ccbcb78b79ccae4eee0a0a9203617a791fe32014
profsibz3090 <sibandadonty@gmail.com>
2025-01-19 16:39:10 +0200

    basic styling to the navbar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
 
 function NavBar() {
+  const [showNav, setShowNav] = useState(false);
+  
   return (
-    <nav>
+    <nav className="flex justify-between px-8 py-4 bg-primary text-white">
       <h3>Donty Sibanda</h3>
 
       {/* Mobile Navigation */}
-      <ul>
+      <ul className="flex gap-2 md:hidden absolute top-0 left-0 bottom-0 flex-col p-4 bg-white w-[50%] text-black">
+        <li>
+          <a href="#">Donty Sibanda</a>
+        </li>
         <li>
           <a href="#">Services</a>
         </li>
@@ -22,12 +27,12 @@ function NavBar() {
         </li>
       </ul>
 
-      <div>
-        <CiMenuBurger />
+      <div className="md:hidden">
+        <CiMenuBurger size={24} />
       </div>
 
       {/* Desktop Navigation */}
-      <ul>
+      <ul className="hidden md:flex gap-2">
         <li>
           <a href="#">Services</a>
         </li>
